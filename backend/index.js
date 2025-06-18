47bb19f24c00a55fa2b63067b8798222fec0da93
Victoria Hernandez <vlhernan@meta.com>
2025-06-18 09:46:06 -0700

    enabled cors on the backend server

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
